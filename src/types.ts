8cea3700b3ed0fd38f4a6d754419e1c5faecb08d
plrs9816 <61955474+plrs9816@users.noreply.github.com>
2023-07-29 00:14:49 +0900

    docs: add jsdoc

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,26 @@
+import { createModal } from "./actions";
+import { createManagedModal } from "./helpers";
+
 export interface SlickModalState {
   key: React.Key;
 }
 
 export interface SlickModalCreateOptions {
+  /**
+   * Unique key for the modal. If not provided, a random key will be generated.
+   */
   key?: React.Key;
+  /**
+   * Whether to throw an error when opening modal if a modal with the same key already exists.
+   */
   throwOnDuplicate?: boolean;
+  /**
+   * Whether to preserve previous promise callbacks when opening modal if a modal with the same key already exists.
+   */
   preserveCallbacks?: boolean;
+  /**
+   * Whether to close all other modals in the store when current one is opened.
+   */
   closeOthers?: boolean;
 }
 
@@ -17,10 +32,31 @@ export interface SlickModalCallbacks {
   };
 }
 
+export type ManagedModal = ReturnType<typeof createManagedModal>;
+
 export interface SlickModalHandler<ResolveReturn extends unknown = unknown> {
-  stackIndex: number;
   key: React.Key;
+  /**
+   * Hide (remove) the modal.
+   */
   hide: () => void;
+  /**
+   * Resolve the promise returned by {@link createModal | createModal} or {@link ManagedModal.open | open}.
+   * @param arg the argument to pass to the promise.
+   */
   resolve: (arg: ResolveReturn) => void;
+  /**
+   * Reject the promise returned by {@link createModal | createModal} or {@link ManagedModal.open | open}.
+   * @param arg the argument to pass to the promise.
+   */
   reject: (arg: unknown) => void;
+  /**
+   * Helper to run both resolve and hide.
+   */
+  resolveHide: (arg: ResolveReturn) => void;
+  /**
+   * Helper to run both reject and hide.
+   */
+  rejectHide: (arg: unknown) => void;
+  stackIndex: number;
 }
